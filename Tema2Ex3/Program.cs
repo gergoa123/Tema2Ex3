94b5f8b4378b7718c5ce9d1c1719f557953f6bb1
Gergoa123 <gergo3124@gmail.com>
2022-05-08 12:43:38 +0300

    Add project files.

---
+++ Tema2Ex3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Tema2Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti numarul");
+            int numar = int.Parse(Console.ReadLine());
+            int aux = 0;
+            int original = numar;
+            while (numar > 0)
+            {
+                int x = numar % 10;
+                aux = aux * 10 + x;
+                numar = numar / 10;
+            }
+            if (original == aux)
+            {
+                Console.WriteLine("numarul este palindrom");
+            }
+            else
+            {
+                Console.WriteLine("nu este palindrom");
+            }
+             
+
+        }
+    }
+}
